d938822c7049fc6eb028b09fc9c891c07ee11906
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2024-01-17 15:38:44 +0100

    Add foreground and background colors, implement and render  crypto component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Crypto } from "@/components/crypto";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-4xl text-orange-700 font-semibold text-center mt-12">
-      Welcome to Cryptracka
+    <div className="py-4 mt-12">
+      <h1 className="px-4 text-3xl text-[#f0b90b] font-medium">Cryptracka</h1>
+      <div className="px-4 mx-auto">
+        <Crypto />
+      </div>
     </div>
   );
 }
---
+++ components/crypto.tsx
@@ -0,0 +1,30 @@
+export const Crypto = () => {
+  return (
+    <a
+      href="#"
+      className="py-3 w-full flex justify-between items-center text-base font-normal"
+    >
+      <div className="flex w-[45%]">
+        <div className="flex items-center">
+          <div className="mr-1 ml-2 text-lg text-[#eaecef] font-medium">
+            BTC
+          </div>
+          <div className="text-sm text-[#848E9C] whitespace-nowrap leading-[20px]">
+            Bitcoin
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-end w-[25%] font-medium">
+        <div className="flex flex-col items-end">
+          <span className="text-[#eaecef] font-medium">$4,345</span>
+          <span className="text-xs font-light text-[#848E9C]">$4,345</span>
+        </div>
+      </div>
+      <div className="flex flex-end w-[25%]">
+        <div className="flex items-center justify-center w-[72px] h-10 bg-[#f6465d] rounded">
+          <span className="text-[#eaecef]">-1.28%</span>
+        </div>
+      </div>
+    </a>
+  );
+};
