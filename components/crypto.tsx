ad0bda3d5ab7c1ed31ae995e0815c7b1934a72b0
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2024-01-17 22:44:40 +0100

    Refactor crypto component to display crypto price up to 6sf, add + sign to positive percentage

--- components/crypto.tsx
+++ components/crypto.tsx
@@ -31,16 +31,20 @@ export const Crypto = ({ data }: Props) => {
         </div>
       </div>
       <div className="w-1/3 font-medium text-end">
-        <p className="text-[#eaecef] font-medium">${price.toFixed(2)}</p>
+        <p className="text-[#eaecef] font-medium">
+          ${price < 0.001 ? price.toFixed(6) : price.toFixed(2)}
+        </p>
         <p className="text-xs font-light text-[#848E9C]">${price.toFixed(2)}</p>
       </div>
       <div className="flex justify-end">
         <div
-          className={`flex text-sm items-center justify-center self-end w-[64px] h-8 ${
+          className={`flex text-sm items-center justify-center self-end w-[72px] h-8 ${
             changePrc > 0 ? "bg-[#0ecb81]" : "bg-[#f6465d]"
           } rounded`}
         >
-          <span>{changePrc.toFixed(2)}%</span>
+          <span>
+            {changePrc > 0 ? `+${changePrc.toFixed(2)}` : changePrc.toFixed(2)}%
+          </span>
         </div>
       </div>
     </a>
