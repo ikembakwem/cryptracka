613226f7c9bde3e5b2ef05620bc955636d5904e7
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2024-01-17 14:03:35 +0100

    Fix: Vercel overwrites favicon.ico file. Use icon metadata property to set favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Cryptracka",
   description: "Monitor all your favorites cryptocurrencies in one place",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
