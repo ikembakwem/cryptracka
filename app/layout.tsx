402db4bc16167798395be23387bf2a3f9ffb8f16
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2024-01-17 13:20:11 +0100

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Cryptracka",
+  description: "Monitor all your favorites cryptocurrencies in one place",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <header></header>
+        <main>{children}</main>
+        <footer></footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-4xl text-orange-700 font-semibold text-center mt-12">
+      Welcome to Cryptracka
+    </div>
+  );
+}
