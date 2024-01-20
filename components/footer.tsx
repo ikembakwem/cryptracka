2421dfbc0af07061a9151bfa20b93064bd82f130
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2024-01-20 19:02:20 +0100

    Remove margin for copyright text on medium screens and align footer text to baseline

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
     <footer className="mt-24 border-t border-[#36363d]">
       <Container>
-        <div className="flex flex-col-reverse md:flex-row md:justify-between py-12">
-          <div className="font-light text-sm text-[#909baa] mt-3">
+        <div className="flex flex-col-reverse md:flex-row md:justify-between py-12 items-baseline">
+          <div className="font-light text-sm text-[#909baa] mt-3 md:mt-0">
             Copyright &copy; {new Date().getFullYear()} Ikechukwu Mbakwem
           </div>
           <div>Powered by CoinCapAPI</div>
